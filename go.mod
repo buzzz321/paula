0739283a7f35da7a6dcef56adfd58749b48ca606
Anders Olme <anders.olme@gmail.com>
2020-08-03 17:47:37 +0200

    First checkin
    Trying to mage a chatbot for amigaswe

---
+++ go.mod
@@ -0,0 +1,11 @@
+module paula
+
+go 1.14
+
+require (
+	github.com/bwmarrin/discordgo v0.21.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	golang.org/x/crypto v0.0.0-20200709230013-948cd5f35899 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
+)
---
+++ paula.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+type whatIs struct {
+	who   string
+	entry string
+}
+
+var (
+	whatisDb []whatIs
+	mutex    = &sync.Mutex{}
+)
+
+func readkey(filename string) string {
+	inputFile, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer inputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
+	scanner.Scan()
+	key := scanner.Text()
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+	return key
+}
+
+func setWhatis(name string, message string) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	fmt.Println(name + " " + message)
+	whatisDb = append(whatisDb, whatIs{name, message})
+
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	//bail out if not a bot command
+	if m.Content[0] != '!' {
+		return
+	}
+
+	splitted := strings.SplitN(m.Content, " ", 2)
+
+	// well we need to have a command at least
+	if len(splitted) < 1 {
+		return
+	}
+	cmd := splitted[0]
+	rest := splitted[1]
+
+	fmt.Println("saldjasdlkasj")
+	// If the message is "ping" reply with "Pong!"
+	if cmd == "!randwhatis" {
+		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	}
+
+	// If the message is "pong" reply with "Ping!"
+	if cmd == "!setwhatis" {
+		setWhatis(m.Author.Username, rest)
+
+		s.ChannelMessageSend(m.ChannelID, "Ping!")
+	}
+}
+
+func main() {
+	discordKey := readkey("../../../discord/paula.key")
+
+	dg, err := discordgo.New("Bot " + discordKey)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+
+}
